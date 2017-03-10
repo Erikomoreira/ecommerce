df3b39bfb9cfe5d989cc348c4feaa49bba54112d
Sibele Nepomuceno <snepomuceno@indracompany.com>
2017-03-10 19:49:55 -0300

    Pequeno ajuste na classe categoriaDAO

--- src/main/resources/ecommerce/Model/Dao/CategoriaDAO.java
+++ src/main/resources/ecommerce/Model/Dao/CategoriaDAO.java
@@ -1,13 +1,9 @@
 package ecommerce.Model.Dao;
 
-import ecommerce.Model.Accessormethod.Categoria;
-import static ecommerce.Model.Dao.ConexaoBD.getConnection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
 
 /*  @author sibele */
 public class CategoriaDAO {
 
- 
+// A fazer
 }
